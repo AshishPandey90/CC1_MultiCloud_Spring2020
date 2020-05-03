4796e2d8f06c604c3893446012d5cedef5affb69
Kolton Speer <kpsgf7@github.com>
2020-05-03 15:15:40 -0500

    super basic addition of form processing into post and get requests. Will finish the details after Ashish posts the endpoint

--- MultiCloudQuestionnaires/src/app/tab1/tab1.page.ts
+++ MultiCloudQuestionnaires/src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -9,8 +11,9 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 export class Tab1Page {
 
   public cloudPreferencesForm: FormGroup;
+  private form_vals;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
 
     this.cloudPreferencesForm = this.fb.group({
       expertise: ['', Validators.required],
@@ -24,7 +27,27 @@ export class Tab1Page {
 
   // called when submit button is clicked
   submitSurvey() {
+    //console.log("REQUESTING DATA");
+    //this.http.get("https://dog.ceo/api/breeds/image/random").subscribe(response => console.log(response));
+ 
 
+    console.log("POSTING DATA");
+    this.form_vals = this.cloudPreferencesForm.value;
+
+    this.http.post("https://google.com", {user_id: "kpsgf7", 
+                                          user_session: "kspgf7_4212020214PM",
+                                          scale: "5",
+                                          scale_identifiers:["Very_Poor","Below_Average","Average","Above_Average","Excellent"],
+                                          csp_identifiers:["Geni","AWS","MU"],
+                                          user_expertise: this.form_vals.expertise,
+                                          criteria:{
+                                            performance:this.form_vals.performance,
+                                            agility:this.form_vals.agility,
+                                            cost: this.form_vals.cost,
+                                            security: this.form_vals.security
+                                          },
+                                          rules:[]
+                                          }).subscribe(response => console.log(response))
   }
 
 }

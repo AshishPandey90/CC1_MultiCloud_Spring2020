5e330b197fa3ca267a5a00a6d3a2d2957ad06cf3
Claire Hough <claire.n.hough@gmail.com>
2020-05-02 21:11:25 -0500

    rule creation form done

--- MultiCloudQuestionnaires/src/app/tab3/tab3.page.ts
+++ MultiCloudQuestionnaires/src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  rules = [
+    {type: "Performance"},
+    {type: "Agility"},
+    {type: "Security"},
+    {type: "Cost"}
+  ];
+
+  choices = [
+    {phrase: "Excellent"},
+    {phrase: "Above Average"},
+    {phrase: "Average"},
+    {phrase: "Below Average"},
+    {phrase: "Poor"}
+  ];
+
+  public ruleCreationForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder) {
+
+    this.ruleCreationForm = this.fb.group({
+      performance: ['', Validators.required],
+      agility: ['', Validators.required],
+      cost: ['', Validators.required],
+      security: ['', Validators.required],
+      CSP: ['', Validators.required]
+    });
+
+  }
+
+  submitSurvey(){
+
+  }
 
 }
